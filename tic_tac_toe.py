989977a1d013fec2892b415b212c7080c75ec313
Emerson Raniere de Macedo <56753061+emersonmacedo123@users.noreply.github.com>
2021-05-03 19:11:02 +0200

    Adding the file with init_board+print_board

---
+++ tic_tac_toe.py
@@ -0,0 +1,73 @@
+def init_board():
+    rowA = [".", ".", "."]
+    rowB = [".", ".", "."]
+    rowC = [".", ".", "."]
+    board = [rowA, rowB, rowC]
+    return board
+
+
+def get_move(board, player):
+    """Returns the coordinates of a valid move for player on board."""
+    row, col = 0, 0
+    return row, col
+
+
+def get_ai_move(board, player):
+    """Returns the coordinates of a valid move for player on board."""
+    row, col = 0, 0
+    return row, col
+
+
+def mark(board, player, row, col):
+    """Marks the element at row & col on the board for player."""
+    pass
+
+
+def has_won(board, player):
+    """Returns True if player has won the game."""
+    return False
+
+
+def is_full(board):
+    """Returns True if board is full."""
+    return False
+
+
+def print_board(board):
+    print("   1   2   3   ")
+    print("   " + board[0][0] + " | " + board[0][1]  + " | " + board[0][2]  )
+    print("  ---+---+---")
+    print("   " + board[1][0] + " | " + board[1][1]  + " | " + board[1][2]  )
+    print("  ---+---+---")
+    print("   " + board[2][0] + " | " + board[2][1]  + " | " + board[2][2]  )
+    pass
+
+
+def print_result(winner):
+    """Congratulates winner or proclaims tie (if winner equals zero)."""
+    pass
+
+'''
+def tictactoe_game(mode='HUMAN-HUMAN'):
+    board = init_board()
+
+    # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
+    print_board(board)
+    row, col = get_move(board, 1)
+    mark(board, 1, row, col)
+
+    winner = 0
+    print_result(winner)
+
+
+def main_menu():
+    tictactoe_game('HUMAN-HUMAN')
+
+
+if __name__ == '__main__':
+    main_menu()
+'''
+############################
+#Testing purpose only
+init_board = init_board()
+print_board(init_board)
