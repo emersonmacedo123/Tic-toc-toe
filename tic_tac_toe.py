cd14df4a531e19ac73cef1fe32909961daa2986f
Emerson Raniere de Macedo <56753061+emersonmacedo123@users.noreply.github.com>
2021-05-06 22:15:35 +0200

    Project finished. Missing AI

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,19 +1,26 @@
+def clean_terminal():
+    import os
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def init_board():
-    rowA = ["x", ".", "."]
-    rowB = [".", "x", "."]
-    rowC = [".", ".", "x"]
+    rowA = [".", ".", "."]
+    rowB = [".", ".", "."]
+    rowC = [".", ".", "."]
     board = [rowA, rowB, rowC]
     return board
 
 def validate_user_input(input):
     valid = False
+    #Check if in board range
     if len(input) == 2:
         if input[0] in ["A", "B", "C"] and input[1] in ["1", "2", "3"]:
             valid = True
         else:
-            print('Wrong coordinates')
+            print('Coordinates out of range')
     else:
         print('Please enter valid coordinates')
+    
+
     return valid
 
 
@@ -44,15 +51,18 @@ def get_ai_move(board, player):
 
 
 def mark(board, player, row, col):
+    valid_mark = False
     if board[row][col] == ".":
         if player == "P1":
             board[row][col] = "X"
         else:
             board[row][col] = "0"
+        valid_mark = True
     else:
-        #do nothing
-        pass
-    return board
+        print("Cell is occupied. Please try again.")
+        valid_mark = False
+        
+    return board, valid_mark
 
 
 
@@ -61,7 +71,7 @@ def has_won(board, player):
     #checking line
     while i <= 2:
         if board[i][0] != "." and board[i][0] == board[i][1] == board[i][2]:
-            print("has winner line")
+            print("Player " + player + " has completed the line. Congrats!")
             return True
             break
         i+=1
@@ -69,35 +79,41 @@ def has_won(board, player):
     j=0
     while j <= 2:
         if board[0][j] != "." and board[0][j] == board[1][j] == board[2][j]:
-            print("has winner column")
+            print("Player " + player + " has completed the column. Congrats!")
             return True
             break
         j+=1
     #checking crossing
     if board[1][1] != "." and (board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]):
-        print("Has winner cross")
+        print("Player" + player + " won. Congrats!")
         return True
     
     #else: no winner
     print("Has no winner")
     return False
 
-'''
 def is_full(board):
-   for i in range(1,10):
-       if has_won(board, i):
-           return True
-    return False
-'''
+    i = 0
+    while i <= 2:
+        j = 0
+        while j <= 2:
+            if board[i][j] == ".":
+                return False
+            j+=1
+        i+=1
+    
+    return True
+
+
 
 def print_board(board):
-    print("   1   2   3   ")
+    print("    1   2   3   ")
     print("A   " + board[0][0] + " | " + board[0][1]  + " | " + board[0][2]  )
     print("  ---+---+---")
     print("B   " + board[1][0] + " | " + board[1][1]  + " | " + board[1][2]  )
     print("  ---+---+---")
     print("C   " + board[2][0] + " | " + board[2][1]  + " | " + board[2][2]  )
-    pass
+    
 
 
 def print_result(player, winner, tie):
@@ -108,26 +124,53 @@ def print_result(player, winner, tie):
     elif tie is True:
         print("...And there's a tie :/ ")
 
+def change_player(player):
+    new_player = ""
+    if player == "P1":
+        new_player = "P2"
+    elif player == "P2":
+        new_player = "P1"
+    return new_player 
 
 
-'''
+
 def tictactoe_game(mode='HUMAN-HUMAN'):
-    board = init_board()
-
-    # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
-    print_board(board)
-    row, col = get_move(board, 1)
-    mark(board, 1, row, col)
-
-    winner = 0
-    print_result(winner)
-
-
+    clean_terminal()
+    print("Welcome to Tic-Toc-Toe game.")
+    replay = True
+    while replay == True:
+        board = init_board()
+        player = "P1" 
+        gameContinue = True
+        while gameContinue == True:
+            print_board(board)
+            occupied_check = False
+            while occupied_check == False:
+                row, col = get_move()
+                [board, validity] = mark(board, player, row, col)
+                occupied_check = validity
+            
+            print_board(board)
+            if has_won(board, player) == True:
+                #We have a winner. End.
+                gameContinue = False
+            elif is_full(board) == True:
+                #We have a tie. End. 
+                gameContinue = False
+            clean_terminal()
+            player=change_player(player)
+        print_board(board)
+        if gameContinue == False:
+            replay_trigger = input("Game is over. Would you like to play again? (Y/N)")
+            clean_terminal()
+            if replay_trigger == "Y":
+                print("The game will restart.")
+            elif replay_trigger == "N":
+                print("Game is over. Thank you.")
+                replay = False
+    
 def main_menu():
     tictactoe_game('HUMAN-HUMAN')
 
-
 if __name__ == '__main__':
-    main_menu()
-'''
-
+    main_menu() 
